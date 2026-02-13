a9180aa60ae6db180e74a626248fc55814529f26
Gab-Zag <gab.alves.pereira30@gmail.com>
2026-02-13 02:52:18 -0300

    feat : add base project

---
+++ src/main/java/com/Personal_Blogging_Platform_API/pbap/PbapApplication.java
@@ -0,0 +1,13 @@
+package com.Personal_Blogging_Platform_API.pbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Personal_Blogging_Platform_API/pbap/PbapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Personal_Blogging_Platform_API.pbap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
